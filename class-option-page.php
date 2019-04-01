8fa364af0cb318e6bbc4418632c15ecdc912c87a
TruongTuyen <hieudieuluong@gmail.com>
2019-04-01 23:40:38 +0700

    Initial commit

---
+++ class-option-page.php
@@ -0,0 +1,153 @@
+<?php
+
+class TeeSightOrders {
+	private $options;
+
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'orders_add_plugin_page' ) );
+		add_action( 'admin_init', array( $this, 'orders_page_init' ) );
+	}
+
+	public function orders_add_plugin_page() {
+		add_menu_page(
+			esc_html__( 'TeeSight Orders', 'teesight-sync-orders' ),
+			esc_html__( 'TeeSight Orders', 'teesight-sync-orders' ),
+			'manage_options',
+			'teesight-orders',
+			array( $this, 'orders_create_admin_page' ),
+			'dashicons-admin-generic',
+			76
+		);
+	}
+
+	public function orders_create_admin_page() {
+		$this->options = get_option( 'teesight_order_options' ); ?>
+
+		<div class="wrap">
+			<h2><?php esc_html_e( 'TeeSight Orders', 'teesight-sync-orders' ); ?></h2>
+			<p><?php esc_html_e( 'TeeSight Sync Orders', 'teesight-sync-orders' ); ?></p>
+			<?php settings_errors(); ?>
+
+			<form method="post" action="options.php">
+				<?php
+					settings_fields( 'orders_option_group' );
+					do_settings_sections( 'teesight-orders-admin' );
+					submit_button();
+				?>
+			</form>
+		</div>
+	<?php }
+
+	public function orders_page_init() {
+		register_setting(
+			'orders_option_group',
+			'teesight_order_options',
+			array( $this, 'orders_sanitize' )
+		);
+
+		add_settings_section(
+			'orders_setting_section',
+			esc_html__( 'Settings', 'teesight-sync-orders' ),
+			array( $this, 'orders_section_info' ),
+			'teesight-orders-admin'
+		);
+
+		add_settings_field(
+			'site_name',
+			esc_html__( 'Site Name', 'teesight-sync-orders' ),
+			array( $this, 'site_name_callback' ),
+			'teesight-orders-admin',
+			'orders_setting_section'
+		);
+
+		add_settings_field(
+			'site_address',
+			esc_html__( 'Site Address', 'teesight-sync-orders' ),
+			array( $this, 'site_address_callback' ),
+			'teesight-orders-admin',
+			'orders_setting_section'
+		);
+
+		add_settings_field(
+			'consumer_key',
+			esc_html__( 'Consumer key', 'teesight-sync-orders' ),
+			array( $this, 'consumer_key_callback' ),
+			'teesight-orders-admin',
+			'orders_setting_section'
+		);
+
+		add_settings_field(
+			'consumer_secret',
+			esc_html__( 'Consumer secret', 'teesight-sync-orders' ),
+			array( $this, 'consumer_secret_callback' ),
+			'teesight-orders-admin',
+			'orders_setting_section'
+		);
+	}
+
+	public function orders_sanitize( $input ) {
+		$sanitary_values = array();
+		if ( isset( $input['site_name'] ) ) {
+			$sanitary_values['site_name'] = sanitize_text_field( $input['site_name'] );
+		}
+
+		if ( isset( $input['site_address'] ) ) {
+			$sanitary_values['site_address'] = sanitize_text_field( $input['site_address'] );
+		}
+
+		if ( isset( $input['consumer_key'] ) ) {
+			$sanitary_values['consumer_key'] = sanitize_text_field( $input['consumer_key'] );
+		}
+
+		if ( isset( $input['consumer_secret'] ) ) {
+			$sanitary_values['consumer_secret'] = sanitize_text_field( $input['consumer_secret'] );
+		}
+
+		return $sanitary_values;
+	}
+
+	public function orders_section_info() {
+
+	}
+
+	public function site_name_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="teesight_order_options[site_name]" id="site_name" value="%s">',
+			isset( $this->options['site_name'] ) ? esc_attr( $this->options['site_name'] ) : ''
+		);
+	}
+
+	public function site_address_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="teesight_order_options[site_address]" id="site_address" value="%s">',
+			isset( $this->options['site_address'] ) ? esc_attr( $this->options['site_address'] ) : ''
+		);
+	}
+
+	public function consumer_key_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="teesight_order_options[consumer_key]" id="consumer_key" value="%s">',
+			isset( $this->options['consumer_key'] ) ? esc_attr( $this->options['consumer_key'] ) : ''
+		);
+	}
+
+	public function consumer_secret_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="teesight_order_options[consumer_secret]" id="consumer_secret" value="%s">',
+			isset( $this->options['consumer_secret'] ) ? esc_attr( $this->options['consumer_secret'] ) : ''
+		);
+	}
+
+}
+if ( is_admin() ) {
+	$orders = new TeeSightOrders();
+}
+
+/*
+ * Retrieve this value with:
+ * $options = get_option( 'teesight_order_options' ); // Array of All Options
+ * $site_name = $options['site_name']; // Site Name
+ * $site_address = $options['site_address']; // Site Address
+ * $consumer_key = $options['consumer_key']; // Consumer key
+ * $consumer_secret = $options['consumer_secret']; // Consumer secret
+ */
---
+++ class-sync-orders.php
@@ -0,0 +1,101 @@
+<?php
+use Automattic\WooCommerce\Client;
+class TeeSight_Sync_Order {
+	protected $woocommerce = null;
+
+	public function __construct() {
+		add_action( 'woocommerce_thankyou', array( $this, 'sync_order' ), PHP_INT_MAX, 1 );
+		$settings = get_option( 'teesight_order_options' );
+		if ( isset( $settings['site_address'] ) && isset( $settings['consumer_key'] ) && isset( $settings['consumer_secret'] ) ) {
+			$this->woocommerce = new Client(
+				$settings['site_address'],
+				$settings['consumer_key'],
+				$settings['consumer_secret'],
+				array(
+					'wp_api' => true,
+					'version' => 'wc/v3',
+					'verify_ssl' => false,
+				)
+			);
+		}
+	}
+
+	public function sync_order( $order_id ) {
+		$order = wc_get_order( $order_id );
+		$deep = 2;
+		if ( false === $order ) {
+			return false;
+		}
+		if ( null !== $this->woocommerce ) {
+			$data = array(
+				'payment_method' => $order->get_payment_method(),
+				'payment_method_title' => $order->get_payment_method_title(),
+				'set_paid' => false,
+				'status'    => 'pending',
+				'currency' => $order->get_currency(),
+				'date_created' => $order->get_date_created()->date( 'Y-m-d H:i:s' ),
+				'date_modified' => $order->get_date_modified()->date( 'Y-m-d H:i:s' ),
+				'discount_total' => (string) wc_format_decimal( $order->get_total_discount(), $deep ),
+				'shipping_total' => (string) wc_format_decimal( $order->get_total_shipping(), $deep ),
+				'total' => (string) wc_format_decimal( $order->get_total(), $deep ),
+				'total_tax' => (string) wc_format_decimal( $order->get_total_tax(), $deep ),
+				'order_key' => $order->get_order_key(),
+				'billing' => array(
+					'first_name' => $order->get_billing_first_name(),
+					'last_name' => $order->get_billing_last_name(),
+					'address_1' => $order->get_billing_address_1(),
+					'address_2' => $order->get_billing_address_2(),
+					'city' => $order->get_shipping_city(),
+					'state' => $order->get_billing_state(),
+					'postcode' => $order->get_billing_postcode(),
+					'country' => $order->get_billing_country(),
+					'email' => $order->get_billing_email(),
+					'phone' => $order->get_billing_phone(),
+				),
+				'shipping' => array(
+					'first_name' => $order->get_shipping_first_name(),
+					'last_name' => $order->get_shipping_last_name(),
+					'address_1' => $order->get_shipping_address_1(),
+					'address_2' => $order->get_shipping_address_2(),
+					'city' => $order->get_shipping_city(),
+					'state' => $order->get_shipping_state(),
+					'postcode' => $order->get_shipping_postcode(),
+					'country' => $order->get_shipping_country(),
+				),
+			);
+
+			foreach ( $order->get_items() as $item_id => $item ) {
+				$product = $item->get_product();
+				$product_id = null;
+				$product_sku = null;
+				if ( is_object( $product ) ) {
+					$product_id = $product->get_id();
+					$product_sku = $product->get_sku();
+				}
+				$data['line_items'][] = array(
+					'name' => $item['name'],
+					'product_id' => ( ! empty( $item->get_variation_id() ) && ( 'product_variation' === $product->post_type ) ) ? $product->get_parent_id() : $product_id,
+					'variation_id' => ( ! empty( $item->get_variation_id() ) && ( 'product_variation' === $product->post_type ) ) ? $product_id : 0,
+					'quantity' => wc_stock_amount( $item['qty'] ),
+					'subtotal' => (string) wc_format_decimal( $order->get_line_subtotal( $item, false, false ), $deep ),
+					'subtotal_tax' => (string) wc_format_decimal( $item['line_subtotal_tax'], $deep ),
+					'total' => (string) wc_format_decimal( $order->get_line_total( $item, false, false ), $deep ),
+					'total_tax' => (string) wc_format_decimal( $item['line_tax'], $deep ),
+					'price' => (string) wc_format_decimal( $order->get_item_total( $item, false, false ), $deep ),
+					'sku' => (string) $product_sku,
+				);
+			}
+
+			foreach ( $order->get_shipping_methods() as $shipping_item_id => $shipping_item ) {
+				$data['shipping_lines'][] = array(
+					'method_id' => $shipping_item['method_id'],
+					'method_title' => $shipping_item['name'],
+					'total' => (string) wc_format_decimal( $shipping_item['cost'], $deep ),
+				);
+			}
+			$result = $this->woocommerce->post( 'orders', $data );
+		}
+	}
+}
+
+new TeeSight_Sync_Order();
